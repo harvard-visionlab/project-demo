12411e85337db1b1062572d2f171510835fc5fed
grez72 <grez72@gmail.com>
2026-01-16 14:54:23 -0500

    renamed project

--- check_install.py
+++ check_install.py
@@ -1,9 +1,15 @@
-import torch, sys
-import visionlab.project_template as m, inspect
+from visionlab.datasets import StreamingDataset
+import pandas as pd
+import cv2
+import litdata
+import torch
+import sys
+import visionlab.project_demo as m
+import inspect
 
 print("✅ import successful")
 print("module file:", m.__file__)
-print("is from working tree? ->", "project_template" in m.__file__)
+print("is from working tree? ->", "project_demo" in m.__file__)
 
 print("torch:", torch.__version__)
 print("torch cuda runtime tag:", torch.version.cuda)
@@ -12,14 +18,10 @@ print("cuda available:", torch.cuda.is_available())
 device = torch.cuda.get_device_name(0) if torch.cuda.is_available() else 'cpu'
 print("device:", device)
 
-import litdata
 print("litdata version:", litdata.__version__)
 
-import cv2
 print("cv2 version:", cv2.__version__)
 
-import pandas as pd
 print("pandas version:", pd.__version__)
 
-from visionlab.datasets import StreamingDataset
 print("StreamingDataset:", StreamingDataset)
